0bae8cb872617d230fe7f25276253ff589447fb3
Anthony <anthonybrooks226@gmail.com>
2019-09-11 15:30:57 -0700

    fixed some syntax issues in cli and scraper

--- lib/barnes_and_noble_bestsellers/cli_bestsellers.rb
+++ lib/barnes_and_noble_bestsellers/cli_bestsellers.rb
@@ -1,11 +1,14 @@
+# frozen_string_literal: true
+
+# creates the main interface for running the program
+# also logic to use the program effectively
 class CLI
-  
   def run
-    puts ""
-    puts "Welcome to the Barnes and Noble Bestsellers Scraper!"
-    puts ""
-    puts "Select a book by number to view more details."
-    puts ""
+    puts ''
+    puts 'Welcome to the Barnes and Noble Bestsellers Scraper!'
+    puts ''
+    puts 'Select a book by number to view more details.'
+    puts ''
     Scraper.scrape_books
     menu
   end
@@ -15,57 +18,47 @@ class CLI
       puts "#{index + 1}. #{book.title}"
     end
 
-    puts ""
-    puts "Please select a book by number to view more information"
+    puts ''
+    puts 'Please select a book by number to view more information'
 
-    input = gets.chomp
+    @input = gets.chomp
+    puts ''
+    puts '###############'
+    puts ''
+    check_input
+    puts ''
+    puts '###############'
+    puts ''
+    completed
+  end
 
-    if input.to_i > Book.all.length || input.to_i <= 0
-      puts ""
-      puts "###############"
-      puts ""
-      puts "Input invalid"
-      puts ""
-      puts "###############"
-      puts ""
-      continue?
+  def check_input
+    if @input.to_i > Book.all.length || @input.to_i <= 0
+      puts 'Input invalid'
     else
-      book = Book.all[input.to_i - 1]
-      puts ""
-      puts "###############"
-      puts ""
+      book = Book.all[@input.to_i - 1]
       puts "Book Title: #{book.title}"
       puts "Author(s): #{book.author}"
       puts "Publication Date: #{book.publish_date}"
       puts "Review Count: #{book.review_count}"
-      puts "#{book.rating}"
+      puts book.rating.to_s
       puts "Format: #{book.format}"
       puts "Barnes and Noble Price: #{book.bn_price}"
       puts "Original Price: #{book.og_price}"
       puts "Barnes and Noble Discount: #{book.discount}"
       puts "Availability: #{book.availability}"
-      puts ""
-      puts "###############"
-      puts ""
-      continue?
     end
-    
   end
 
-  def continue?
-    puts ""
-    puts "Would you like to continue? (y/n)"
-    puts ""
+  def completed
+    puts "\nWould you like to continue? (y/n)"
     answer = gets.chomp
-    while true
-      if answer == 'yes' || answer == 'y'
-        menu
-      elsif answer == 'no' || answer == 'n'
-        exit
-      else
-        continue?
-      end
+    if %w[yes y].include?(answer)
+      menu
+    elsif %w[no n].include?(answer)
+      exit
+    else
+      completed
     end
   end
-
 end
--- lib/barnes_and_noble_bestsellers/scraper.rb
+++ lib/barnes_and_noble_bestsellers/scraper.rb
@@ -1,15 +1,16 @@
-class Scraper
+# frozen_string_literal: true
 
-  @@bn_url = 'https://www.barnesandnoble.com/'
-  
+# gets html from Barnes & Noble
+# creates a new Book for each book in the bestseller list
+class Scraper
+  @bn_url = 'https://www.barnesandnoble.com/'
+
   def self.scrape_books
-    scrape_url = @@bn_url + 'b/books/_/N-1fZ29Z8q8'
+    scrape_url = @bn_url + 'b/books/_/N-1fZ29Z8q8'
 
     doc = Nokogiri::HTML(open(scrape_url))
-    
 
     doc.css('.pb-s.mt-m.bd-bottom-disabled-gray.record').each do |book|
-      
       title = book.css('h3 a').text.strip
       author = book.css('.product-shelf-author.contributors a').text.strip
       publish_date = book.css('span.text--small.publ-date').text.strip
@@ -21,9 +22,10 @@ class Scraper
       discount = book.css('p.discount').text.strip
       availability = book.css('div.availability-spacing').text.strip
 
-      Book.new(title, author, publish_date, review_count, rating, format, bn_price, og_price, discount, availability)
-
-    end 
+      Book.new(title, author, publish_date,\
+               review_count, rating, format,\
+               bn_price, og_price, discount,\
+               availability)
+    end
   end
-
-end 
+end
